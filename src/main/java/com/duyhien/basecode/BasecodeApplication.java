2818e5d67bf3a3d7eec879002e320769cf2ce955
Duy Hien <duyhienn.2012@gmail.com>
2024-06-21 23:52:35 +0700

    Base Code

---
+++ src/main/java/com/duyhien/basecode/BasecodeApplication.java
@@ -0,0 +1,13 @@
+package com.duyhien.basecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasecodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasecodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duyhien/basecode/BasecodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duyhien.basecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasecodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
